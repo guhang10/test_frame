f53d7b8fa63e91baa3a195a18cfcdfffa81b6791
guhang10 <guhang92@gmail.com>
2016-08-25 16:37:09 +1000

    updated vmware_module.py to display data store info

--- vmware_module.py
+++ vmware_module.py
@@ -46,7 +46,7 @@ class vmware_connect_test(base.vmware_base):
 class vmware_get_vms(base.vmware_base):
     description = "show all vms on a vmware host"
 
-    def __init__(self, host, user, password):
+    def __init__(self, host, user, password, json):
         super(vmware_get_vms, self).__init__("vmware_get_vms", "6.0.0")
         self.context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
         self.context.verify_mode = ssl.CERT_NONE
@@ -403,6 +403,127 @@ class vmware_list_datastore_info(base.vmware_base):
     description = "list datastore informations"
 
     def __init__(self, host, user, password, json):
+        super(vmware_list_datastore_info, self).__init__("vmware_list_datastore_info", "6.0.0")
+        self.context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        self.context.verify_mode = ssl.CERT_NONE
+        self.host = host
+        self.user = user
+        self.password = password
+        self.json = json
+
+    def sizeof_fmt(self, num):
+        """
+        Returns the human readable version of a file size
+
+        :param num:
+        :return:
+        """
+        for item in ['bytes', 'KB', 'MB', 'GB']:
+            if num < 1024.0:
+                return "%3.1f%s" % (num, item)
+            num /= 1024.0
+        return "%3.1f%s" % (num, 'TB')
+
+
+    def print_fs(self, host_fs):
+        """
+        Prints the host file system volume info
+
+        :param host_fs:
+        :return:
+        """
+        print("{}\t{}\t".format("Datastore:     ", host_fs.volume.name))
+        print("{}\t{}\t".format("UUID:          ", host_fs.volume.uuid))
+        print("{}\t{}\t".format("Capacity:      ", self.sizeof_fmt(
+            host_fs.volume.capacity)))
+        print("{}\t{}\t".format("VMFS Version:  ", host_fs.volume.version))
+
+
+    def main(self):
+
+        try:
+            service_instance = connect.SmartConnect(host=self.host ,user=self.user,
+                    pwd=self.password, port=443, sslContext=self.context)
+
+            atexit.register(connect.Disconnect, service_instance)
+
+            if not service_instance:
+                print("could not connect ot the host with given credentials")
+                return False
+
+            content = service_instance.RetrieveContent()
+
+            objview = content.viewManager.CreateContainerView(content.rootFolder,
+                                                          [vim.HostSystem],
+                                                          True)
+            esxi_hosts = objview.view
+            objview.Destroy()
+
+            datastores = {}
+
+            for esxi_host in esxi_hosts:
+                if not self.json:
+                    print("{}\t{}\t\n".format("ESXi Host:    ", esxi_host.name))
+
+                # All Filesystems on ESXi host
+                storage_system = esxi_host.configManager.storageSystem
+                host_file_sys_vol_mount_info = \
+                    storage_system.fileSystemVolumeInfo.mountInfo
+
+                datastore_dict = {}
+                # Map all filesystems
+                for host_mount_info in host_file_sys_vol_mount_info:
+                    # Extract only VMFS volumes
+                    if host_mount_info.volume.type == "VMFS":
+
+                        extents = host_mount_info.volume.extent
+                        if not self.json:
+                            self.print_fs(host_mount_info)
+                        else:
+                            datastore_details = {
+                                'uuid': host_mount_info.volume.uuid,
+                                'capacity': host_mount_info.volume.capacity,
+                                'vmfs_version': host_mount_info.volume.version,
+                                'local': host_mount_info.volume.local,
+                                'ssd': host_mount_info.volume.ssd
+                            }
+
+                        extent_arr = []
+                        extent_count = 0
+                        for extent in extents:
+                            if not self.json:
+                                print("{}\t{}\t".format(
+                                    "Extent[" + str(extent_count) + "]:",
+                                    extent.diskName))
+                                extent_count += 1
+                            else:
+                                # create an array of the devices backing the given
+                                # datastore
+                                extent_arr.append(extent.diskName)
+                                # add the extent array to the datastore info
+                                datastore_details['extents'] = extent_arr
+                                # associate datastore details with datastore name
+                                datastore_dict[host_mount_info.volume.name] = \
+                                    datastore_details
+                        if not self.json:
+                            print
+
+                # associate ESXi host with the datastore it sees
+                datastores[esxi_host.name] = datastore_dict
+
+            if self.json:
+                print(json.dumps(datastores))
+
+        except vmodl.MethodFault as error:
+            print("Caught vmodl fault : " + error.msg)
+            return False
+
+        return True
+
+
+
+
+
 
 
 
